e2b24f81cc21c33898563d7fc778c8ff2d84c870
raygu0101 <raygu0101@gmail.com>
2018-08-22 11:07:03 +0800

    [+]添加env.example

---
+++ app/env.example.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'APP_ENV' => 'local',
+    'APP_DEBUG' => true,
+    'APP_KEY' => '',
+
+    'LOG_CHANNEL' => 'stack',
+
+    'DB_CONNECTION' => 'mysql',
+    'DB_HOST' => '127.0.0.1',
+    'DB_PORT' => 3306,
+    'DB_DATABASE' => 'homestead',
+    'DB_USERNAME' => 'homestead',
+    'DB_PASSWORD' => 'secret',
+
+    'CACHE_DRIVER' => 'file',
+    'QUEUE_DRIVER' => 'sync',
+];
